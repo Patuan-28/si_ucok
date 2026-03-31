4d94f64bcd7b5e9d7a16f930a655c9f913361632
gtotz <angga.lkbn@gmail.com>
2026-03-31 16:12:20 +0700

    Add custom logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,26 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ThemeToggle from './ThemeToggle';
+import logoImg from '../assets/si_ucok.png';
 
 export default function Navbar() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   return (
     <nav className="navbar container">
-      <Link to="/" className="logo">Si Ucok</Link>
+      <Link to="/" className="logo" style={{ display: 'flex', alignItems: 'center' }}>
+        <img src={logoImg} alt="Si Ucok Logo" style={{ height: '100px', objectFit: 'contain' }} />
+      </Link>
       <div className="nav-links">
         <Link to="/" className="nav-link">Home</Link>
-        
-        <div 
+
+        <div
           className="nav-dropdown"
           onMouseEnter={() => setDropdownOpen(true)}
           onMouseLeave={() => setDropdownOpen(false)}
         >
           <span className="nav-link" style={{ cursor: 'pointer' }}>Portfolio ▾</span>
-          
+
           {dropdownOpen && (
             <div className="dropdown-menu fade-in">
               <Link to="/frontend" className="dropdown-item">🎨 Frontend UI</Link>
